7703ead66cd88548a55dc83e2d5bc9bc6141fee7
Bernhard Kuzel <bernhard.kuzel@gmail.com>
2013-04-08 08:43:33 +0200

    Added some java doc

--- src/main/java/com/firstblick/elasticsearch/action/export/OutputCommand.java
+++ src/main/java/com/firstblick/elasticsearch/action/export/OutputCommand.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+
+/**
+ * Start an OS Command as a process and push strings to the process'
+ * standard in. Get standard out and standard error messages when
+ * process has finished.
+ */
 public class OutputCommand {
 
     private static final int BUFFER_LEN = 8192;
@@ -20,14 +26,27 @@ public class OutputCommand {
     private Result result;
     private StreamConsumer outputConsumer, errorConsumer;
 
+    /**
+     * Initialize the process builder with a single command.
+     * @param command
+     */
     public OutputCommand(String command) {
         builder = new ProcessBuilder(command);
     }
 
+    /**
+     * Initialize the process with a command list.
+     * @param cmdArray
+     */
     public OutputCommand(List<String> cmdArray) {
         builder = new ProcessBuilder(cmdArray);
     }
 
+    /**
+     * Start the process and prepare writing to it's standard in.
+     *
+     * @throws IOException
+     */
     public void start() throws IOException {
         process = builder.start();
         if (process != null) {
@@ -39,12 +58,25 @@ public class OutputCommand {
         }
     }
 
+    /**
+     * Write a line to the process' standard in.
+     *
+     * @param string
+     */
     public void println(String string) {
         if (printWriter != null) {
             printWriter.println(string);
         }
     }
 
+    /**
+     * Stop writing to the process' standard in and wait until the
+     * process is finished.
+     *
+     * @return a result object conatining the process exit status
+     *         and the forst 8K of the process' output and error log.
+     * @throws IOException
+     */
     public Result end() throws IOException {
         result = new Result();
         if (printWriter != null) {
