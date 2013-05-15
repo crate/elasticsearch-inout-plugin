c9d5b66bbd40e07c937e83bf16480482c06a936c
Bernhard Kuzel <bernhard.kuzel@gmail.com>
2013-05-15 12:02:00 +0200

    check if source is stored on search into, improved java testing

---
+++ src/test/java/crate/elasticsearch/module/searchinto/test/RestSearchIntoActionTest.java
@@ -0,0 +1,42 @@
+package crate.elasticsearch.module.searchinto.test;
+
+import static com.github.tlrx.elasticsearch.test.EsSetup.createIndex;
+import static com.github.tlrx.elasticsearch.test.EsSetup.index;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import crate.elasticsearch.action.searchinto.SearchIntoAction;
+import crate.elasticsearch.action.searchinto.SearchIntoRequest;
+import crate.elasticsearch.action.searchinto.SearchIntoResponse;
+import crate.elasticsearch.module.AbstractRestActionTest;
+
+public class RestSearchIntoActionTest extends AbstractRestActionTest {
+
+    @Test
+    public void testSearchIntoWithoutSource() {
+        esSetup.execute(createIndex("test").withMapping("a",
+                "{\"a\":{\"_source\": {\"enabled\": false}}}"));
+        esSetup.execute(index("test", "a", "1").withSource("{\"name\": \"John\"}"));
+        SearchIntoRequest request = new SearchIntoRequest("test");
+        request.source("{\"fields\": [\"_id\", \"_source\", [\"_index\", \"'newindex'\"]]}");
+        SearchIntoResponse res = esSetup.client().execute(SearchIntoAction.INSTANCE, request).actionGet();
+        assertEquals(1, res.getFailedShards());
+        assertTrue(res.getShardFailures()[0].reason().contains("Parse Failure [The field [_source] of index test and type a is not enabled.]"));
+    }
+
+    private static List<Map<String, Object>> get(SearchIntoResponse resp, String key) {
+        Map<String, Object> res = null;
+        try {
+            res = toMap(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return (List<Map<String, Object>>) res.get(key);
+    }
+
+}
